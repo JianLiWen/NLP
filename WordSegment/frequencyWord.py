be16b4bface29ecfc132f5ecf508a0e410bc7d2a
wjl <JianLiWen>
2019-10-12 11:52:21 +0800

    去除停用词

--- WordSegment/frequencyWord.py
+++ WordSegment/frequencyWord.py
@@ -19,6 +19,11 @@ def get_TF(words, topK=10):
     return sorted(tf_dic.items(), key=lambda x: x[1], reverse=True)[:topK]
 
 
+def stop_words(path):
+    with open(path, encoding='utf-8') as f:
+        return [l.strip() for l in f]
+
+
 def main():
     import glob2
     import random
@@ -26,7 +31,8 @@ def main():
     files = glob2.glob('./data/news/C000013/*.text')   # 所有的路径
     corpus = [get_content(x) for x in files]    # 数组，元素为每一个text的文本内容
     sample_inx = random.randint(0, len(corpus))  # 生成随机数
-    split_words = list(jieba.cut(corpus[1]))   # 分词数组
+    # split_words = list(jieba.cut(corpus[1]))   # 分词数组
+    split_words = [x for x in jieba.cut(corpus[sample_inx]) if x not in stop_words('./data/stop_words.utf8')]
     print('样本之一'+corpus[1])
     print('样本分词效果:'+'/'.join(split_words))
     print('样本的topK词：'+str(get_TF(split_words)))
